f53952c508eafc0120699383c18c154c0922ae6d
Aza Asim <azaasim@gmail.com>
2023-03-28 02:32:02 +0300

    Add UI

--- ToDo.Web/Program.cs
+++ ToDo.Web/Program.cs
@@ -1,11 +1,29 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ShiftSoftware.ShiftBlazor.Extensions;
 using ToDo.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var httpClient = new HttpClient
+{
+    BaseAddress = new Uri(builder.Configuration!.GetValue<string>("BaseURL")!),
+};
+
+builder.Services.AddScoped(sp => httpClient);
+
+builder.Services.AddShiftServices(config =>
+{
+    config.ShiftConfiguration = options =>
+    {
+        options.BaseAddress = builder.Configuration!.GetValue<string>("BaseURL")!;
+        options.ApiPath = "/api";
+        options.ODataPath = "/odata";
+        options.UserListEndpoint = "http://localhost"; //ToDo: this parameter should be optional.
+    };
+    config.SyncfusionLicense = builder.Configuration.GetValue<string>("SyncfusionLicense");
+});
 
 await builder.Build().RunAsync();
