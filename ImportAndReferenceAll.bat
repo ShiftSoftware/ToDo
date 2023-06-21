67f7244fb3a80d8356a464640a6c5dfc62f91edc
Aza Asim <azaasim@gmail.com>
2023-06-21 15:49:15 +0300

    Add Framework Import/Remove Scripts

---
+++ ImportAndReferenceAll.bat
@@ -0,0 +1,44 @@
+dotnet sln add --solution-folder ShiftFramework\ShiftEntity ..\ShiftEntity\ShiftEntity.Model\ShiftEntity.Model.csproj
+dotnet sln add --solution-folder ShiftFramework\ShiftEntity ..\ShiftEntity\ShiftEntity.Core\ShiftEntity.Core.csproj
+dotnet sln add --solution-folder ShiftFramework\ShiftEntity ..\ShiftEntity\ShiftEntity.Web\ShiftEntity.Web.csproj
+dotnet sln add --solution-folder ShiftFramework\ShiftEntity ..\ShiftEntity\ShiftEntity.EFCore.SqlServer\ShiftEntity.EFCore.SqlServer.csproj
+
+dotnet sln add --solution-folder ShiftFramework\ShiftBlazor ..\ShiftBlazor\ShiftBlazor\ShiftBlazor.csproj
+
+dotnet sln add --solution-folder ShiftFramework\TypeAuth ..\TypeAuth\TypeAuth.Core\TypeAuth.Core.csproj
+dotnet sln add --solution-folder ShiftFramework\TypeAuth ..\TypeAuth\TypeAuth.Blazor\TypeAuth.Blazor.csproj
+dotnet sln add --solution-folder ShiftFramework\TypeAuth ..\TypeAuth\TypeAuth.AspNetCore\TypeAuth.AspNetCore.csproj
+
+
+dotnet sln add --solution-folder ShiftFramework\ShiftIdentity ..\ShiftIdentity\ShiftIdentity.Core\ShiftIdentity.Core.csproj
+dotnet sln add --solution-folder ShiftFramework\ShiftIdentity ..\ShiftIdentity\ShiftIdentity.AspNetCore\ShiftIdentity.AspNetCore.csproj
+dotnet sln add --solution-folder ShiftFramework\ShiftIdentity ..\ShiftIdentity\ShiftIdentity.Blazor\ShiftIdentity.Blazor.csproj
+dotnet sln add --solution-folder ShiftFramework\ShiftIdentity ..\ShiftIdentity\ShiftIdentity.Dashboard.AspNetCore\ShiftIdentity.Dashboard.AspNetCore.csproj
+dotnet sln add --solution-folder ShiftFramework\ShiftIdentity ..\ShiftIdentity\ShiftIdentity.Dashboard.Blazor\ShiftIdentity.Dashboard.Blazor.csproj
+
+dotnet add ..\ShiftBlazor\ShiftBlazor\ShiftBlazor.csproj reference ..\ShiftEntity\ShiftEntity.Model\ShiftEntity.Model.csproj
+
+dotnet add ..\ShiftEntity\ShiftEntity.Web\ShiftEntity.Web.csproj reference ..\TypeAuth\TypeAuth.AspNetCore\TypeAuth.AspNetCore.csproj
+
+dotnet add ..\ShiftIdentity\ShiftIdentity.Core\ShiftIdentity.Core.csproj reference ..\ShiftEntity\ShiftEntity.Core\ShiftEntity.Core.csproj
+dotnet add ..\ShiftIdentity\ShiftIdentity.Core\ShiftIdentity.Core.csproj reference ..\TypeAuth\TypeAuth.Core\TypeAuth.Core.csproj
+dotnet add ..\ShiftIdentity\ShiftIdentity.AspNetCore\ShiftIdentity.AspNetCore.csproj reference ..\ShiftEntity\ShiftEntity.Web\ShiftEntity.Web.csproj
+dotnet add ..\ShiftIdentity\ShiftIdentity.AspNetCore\ShiftIdentity.AspNetCore.csproj reference ..\TypeAuth\TypeAuth.Core\TypeAuth.Core.csproj
+dotnet add ..\ShiftIdentity\ShiftIdentity.Dashboard.AspNetCore\ShiftIdentity.Dashboard.AspNetCore.csproj reference ..\ShiftEntity\ShiftEntity.EFCore.SqlServer\ShiftEntity.EFCore.SqlServer.csproj
+
+dotnet add ..\ShiftIdentity\ShiftIdentity.Dashboard.Blazor\ShiftIdentity.Dashboard.Blazor.csproj reference ..\ShiftBlazor\ShiftBlazor\ShiftBlazor.csproj
+dotnet add ..\ShiftIdentity\ShiftIdentity.Dashboard.Blazor\ShiftIdentity.Dashboard.Blazor.csproj reference ..\TypeAuth\TypeAuth.Blazor\TypeAuth.Blazor.csproj
+
+
+
+dotnet add ToDo.Web\ToDo.Web.csproj reference ..\ShiftIdentity\ShiftIdentity.Dashboard.Blazor\ShiftIdentity.Dashboard.Blazor.csproj
+dotnet add ToDo.Web\ToDo.Web.csproj reference ..\ShiftIdentity\ShiftIdentity.Blazor\ShiftIdentity.Blazor.csproj
+dotnet add ToDo.Web\ToDo.Web.csproj reference ..\ShiftBlazor\ShiftBlazor\ShiftBlazor.csproj
+
+
+dotnet add ToDo.Shared\ToDo.Shared.csproj reference ..\ShiftEntity\ShiftEntity.Model\ShiftEntity.Model.csproj
+
+dotnet add ToDo.API\ToDo.API.csproj reference ..\ShiftEntity\ShiftEntity.Web\ShiftEntity.Web.csproj
+dotnet add ToDo.API\ToDo.API.csproj reference ..\ShiftEntity\ShiftEntity.EFCore.SqlServer\ShiftEntity.EFCore.SqlServer.csproj
+dotnet add ToDo.API\ToDo.API.csproj reference ..\ShiftIdentity\ShiftIdentity.Dashboard.AspNetCore\ShiftIdentity.Dashboard.AspNetCore.csproj
+
---
+++ RemoveAll.bat
@@ -0,0 +1,43 @@
+dotnet remove ..\ShiftBlazor\ShiftBlazor\ShiftBlazor.csproj reference ..\ShiftEntity\ShiftEntity.Model\ShiftEntity.Model.csproj
+
+dotnet remove ..\ShiftEntity\ShiftEntity.Web\ShiftEntity.Web.csproj reference ..\TypeAuth\TypeAuth.AspNetCore\TypeAuth.AspNetCore.csproj
+
+dotnet remove ..\ShiftIdentity\ShiftIdentity.Core\ShiftIdentity.Core.csproj reference ..\ShiftEntity\ShiftEntity.Core\ShiftEntity.Core.csproj
+dotnet remove ..\ShiftIdentity\ShiftIdentity.Core\ShiftIdentity.Core.csproj reference ..\TypeAuth\TypeAuth.Core\TypeAuth.Core.csproj
+dotnet remove ..\ShiftIdentity\ShiftIdentity.AspNetCore\ShiftIdentity.AspNetCore.csproj reference ..\ShiftEntity\ShiftEntity.Web\ShiftEntity.Web.csproj
+dotnet remove ..\ShiftIdentity\ShiftIdentity.AspNetCore\ShiftIdentity.AspNetCore.csproj reference ..\TypeAuth\TypeAuth.Core\TypeAuth.Core.csproj
+dotnet remove ..\ShiftIdentity\ShiftIdentity.Dashboard.AspNetCore\ShiftIdentity.Dashboard.AspNetCore.csproj reference ..\ShiftEntity\ShiftEntity.EFCore.SqlServer\ShiftEntity.EFCore.SqlServer.csproj
+
+dotnet remove ..\ShiftIdentity\ShiftIdentity.Dashboard.Blazor\ShiftIdentity.Dashboard.Blazor.csproj reference ..\ShiftBlazor\ShiftBlazor\ShiftBlazor.csproj
+dotnet remove ..\ShiftIdentity\ShiftIdentity.Dashboard.Blazor\ShiftIdentity.Dashboard.Blazor.csproj reference ..\TypeAuth\TypeAuth.Blazor\TypeAuth.Blazor.csproj
+
+
+
+dotnet remove ToDo.Web\ToDo.Web.csproj reference ..\ShiftIdentity\ShiftIdentity.Dashboard.Blazor\ShiftIdentity.Dashboard.Blazor.csproj
+dotnet remove ToDo.Web\ToDo.Web.csproj reference ..\ShiftIdentity\ShiftIdentity.Blazor\ShiftIdentity.Blazor.csproj
+dotnet remove ToDo.Web\ToDo.Web.csproj reference ..\ShiftBlazor\ShiftBlazor\ShiftBlazor.csproj
+
+
+dotnet remove ToDo.Shared\ToDo.Shared.csproj reference ..\ShiftEntity\ShiftEntity.Model\ShiftEntity.Model.csproj
+
+dotnet remove ToDo.API\ToDo.API.csproj reference ..\ShiftEntity\ShiftEntity.Web\ShiftEntity.Web.csproj
+dotnet remove ToDo.API\ToDo.API.csproj reference ..\ShiftEntity\ShiftEntity.EFCore.SqlServer\ShiftEntity.EFCore.SqlServer.csproj
+
+
+dotnet sln remove ..\ShiftEntity\ShiftEntity.Model\ShiftEntity.Model.csproj
+dotnet sln remove ..\ShiftEntity\ShiftEntity.Core\ShiftEntity.Core.csproj
+dotnet sln remove ..\ShiftEntity\ShiftEntity.Web\ShiftEntity.Web.csproj
+dotnet sln remove ..\ShiftEntity\ShiftEntity.EFCore.SqlServer\ShiftEntity.EFCore.SqlServer.csproj
+
+dotnet sln remove ..\ShiftBlazor\ShiftBlazor\ShiftBlazor.csproj
+
+dotnet sln remove ..\TypeAuth\TypeAuth.Core\TypeAuth.Core.csproj
+dotnet sln remove ..\TypeAuth\TypeAuth.Blazor\TypeAuth.Blazor.csproj
+dotnet sln remove ..\TypeAuth\TypeAuth.AspNetCore\TypeAuth.AspNetCore.csproj
+
+
+dotnet sln remove ..\ShiftIdentity\ShiftIdentity.Core\ShiftIdentity.Core.csproj
+dotnet sln remove ..\ShiftIdentity\ShiftIdentity.AspNetCore\ShiftIdentity.AspNetCore.csproj
+dotnet sln remove ..\ShiftIdentity\ShiftIdentity.Blazor\ShiftIdentity.Blazor.csproj
+dotnet sln remove ..\ShiftIdentity\ShiftIdentity.Dashboard.AspNetCore\ShiftIdentity.Dashboard.AspNetCore.csproj
+dotnet sln remove ..\ShiftIdentity\ShiftIdentity.Dashboard.Blazor\ShiftIdentity.Dashboard.Blazor.csproj
