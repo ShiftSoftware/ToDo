f43b067a88ac75a3ece57e59bfaa8c97eaac33ed
Aza Asim <azaasim@gmail.com>
2023-09-01 11:40:48 +0300

    Update Framework Import/Remove script

--- RemoveAll.bat
+++ RemoveAll.bat
@@ -1,21 +1,9 @@
-dotnet remove ToDo.Web\ToDo.Web.csproj reference ..\ShiftIdentity\ShiftIdentity.Dashboard.Blazor\ShiftIdentity.Dashboard.Blazor.csproj
-dotnet remove ToDo.Web\ToDo.Web.csproj reference ..\ShiftIdentity\ShiftIdentity.Blazor\ShiftIdentity.Blazor.csproj
-dotnet remove ToDo.Web\ToDo.Web.csproj reference ..\ShiftBlazor\ShiftBlazor\ShiftBlazor.csproj
-
-
-dotnet remove ToDo.Shared\ToDo.Shared.csproj reference ..\ShiftEntity\ShiftEntity.Model\ShiftEntity.Model.csproj
-dotnet remove ToDo.Shared\ToDo.Shared.csproj reference ..\TypeAuth\TypeAuth.Core\TypeAuth.Core.csproj
-
-dotnet remove ToDo.API\ToDo.API.csproj reference ..\ShiftEntity\ShiftEntity.Web\ShiftEntity.Web.csproj
-dotnet remove ToDo.API\ToDo.API.csproj reference ..\ShiftEntity\ShiftEntity.EFCore.SqlServer\ShiftEntity.EFCore.SqlServer.csproj
-dotnet remove ToDo.API\ToDo.API.csproj reference ..\ShiftIdentity\ShiftIdentity.Dashboard.AspNetCore\ShiftIdentity.Dashboard.AspNetCore.csproj
-
-dotnet remove ToDo.Test\ToDo.Test.csproj reference ..\ShiftFrameworkTestingTools\ShiftFrameworkTestingTools\ShiftFrameworkTestingTools.csproj
 
 dotnet sln remove ..\ShiftEntity\ShiftEntity.Model\ShiftEntity.Model.csproj
 dotnet sln remove ..\ShiftEntity\ShiftEntity.Core\ShiftEntity.Core.csproj
 dotnet sln remove ..\ShiftEntity\ShiftEntity.Web\ShiftEntity.Web.csproj
-dotnet sln remove ..\ShiftEntity\ShiftEntity.EFCore.SqlServer\ShiftEntity.EFCore.SqlServer.csproj
+dotnet sln remove ..\ShiftEntity\ShiftEntity.EFCore\ShiftEntity.EFCore.csproj
+dotnet sln remove ..\ShiftEntity\ShiftEntity.CosmosDbSync\ShiftEntity.CosmosDbSync.csproj
 
 dotnet sln remove ..\ShiftBlazor\ShiftBlazor\ShiftBlazor.csproj
 dotnet sln remove ..\ShiftBlazor\ShiftBlazor.Tests\ShiftBlazor.Tests.csproj
