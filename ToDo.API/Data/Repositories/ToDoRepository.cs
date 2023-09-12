6a6db2742983f3b1056af1e7aeac41f73ddbdb58
Aza Asim <azaasim@gmail.com>
2023-09-12 12:49:25 +0300

    Use ShiftRepositoryOptionBuilder

--- ToDo.API/Data/Repositories/ToDoRepository.cs
+++ ToDo.API/Data/Repositories/ToDoRepository.cs
@@ -1,7 +1,6 @@
 ﻿using AutoMapper;
 using ShiftSoftware.ShiftEntity.Core;
 using ShiftSoftware.ShiftEntity.EFCore;
-using System.Linq.Expressions;
 using ToDo.Shared.DTOs.ToDo;
 
 namespace ToDo.API.Data.Repositories
@@ -10,20 +9,22 @@ namespace ToDo.API.Data.Repositories
         ShiftRepository<DB, Entities.ToDo, ToDoListDTO, ToDoDTO, ToDoDTO>,
         IShiftRepositoryAsync<Entities.ToDo, ToDoListDTO, ToDoDTO>
     {
-        public ToDoRepository(DB db, IMapper mapper) : base(db, db.ToDos, mapper)
+        public ToDoRepository(DB db, IMapper mapper) : base(db, db.ToDos, mapper, x =>
+            x.IncludeRelatedEntitiesWithFindAsync(y => y.Include(z => z.Project))
+        )
         {
         }
 
-        public override async Task<Entities.ToDo> FindAsync(long id, DateTime? asOf = null, Expression<Func<Entities.ToDo, bool>>? where = null)
-        {
-            return await base.FindAsync(id, asOf, where, x => x.Include(y => y.Project));
-        }
+        //public override async Task<Entities.ToDo> FindAsync(long id, DateTime? asOf = null, Expression<Func<Entities.ToDo, bool>>? where = null)
+        //{
+        //    return await base.FindAsync(id, asOf, where, x => x.Include(y => y.Project));
+        //}
 
-        public override async ValueTask<Entities.ToDo> UpsertAsync(Entities.ToDo entity, ToDoDTO dto, ActionTypes actionType, long? userId = null)
-        {
-            entity.ReloadAfterSave = true;
+        //public override async ValueTask<Entities.ToDo> UpsertAsync(Entities.ToDo entity, ToDoDTO dto, ActionTypes actionType, long? userId = null)
+        //{
+        //    entity.ReloadAfterSave = true;
 
-            return await base.UpsertAsync(entity, dto, actionType, userId);
-        }
+        //    return await base.UpsertAsync(entity, dto, actionType, userId);
+        //}
     }
 }
