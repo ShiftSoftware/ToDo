2f7efc2365b7d8d6f6e0da734d25ad2993a9707f
Aza Asim <azaasim@gmail.com>
2023-09-22 02:06:10 +0300

    Update lists to use the new ShiftList

--- ToDo.Shared/Enums/_TaskStatus.cs
+++ ToDo.Shared/Enums/_TaskStatus.cs
@@ -1,9 +1,18 @@
-﻿namespace ToDo.Shared.Enums;
+﻿using System.ComponentModel;
+
+namespace ToDo.Shared.Enums;
 
 public enum _TaskStatus
 {
+    [Description("New")]
     New = 10,
+
+    [Description("In Progress")]
     InProgress = 20,
+
+    [Description("Completed")]
     Completed = 30,
+
+    [Description("Rejected")]
     Rejected = 40,
 }
